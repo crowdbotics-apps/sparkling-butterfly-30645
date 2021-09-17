efc4aa2c8dc4675a78613cd76b10276c7153c75f
Crowdbotics <team@crowdbotics.com>
2021-09-17 17:35:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "sparkling_butterfly_30645Identifier",
+  url: "https://sparkling_butterfly_30645Identifier.botics.co",
+  api: "https://sparkling_butterfly_30645Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
